8be4e96765c8a726f692ae13e14c61e097e98264
Prasenjit <prasenjitnitdgp@gmail.com>
2023-05-01 23:35:14 +0530

    Added Plotly events to get feedback on the OMC Clicked in the bar chart for market share.

--- pages/sa_mkt_sh.py
+++ pages/sa_mkt_sh.py
@@ -2,6 +2,7 @@ import streamlit as st
 from data_analysis import DataAnalysis
 import plotly.express as px
 import plotly.figure_factory as ff
+from streamlit_plotly_events import plotly_events
 
 st.title("SA Market Share Analysis")
 
@@ -18,10 +19,17 @@ with col1:
 with col2:
     chosen_fy = st.multiselect(label="Choose Fiscal Year", options=options_fy)
 
-chosen_product = st.multiselect(label="PRODUCT", options=options_product)
+col1, col2 = st.columns(2)
+with col1:
+    chosen_product = st.multiselect(label="PRODUCT", options=options_product)
+
+with col2:
+    chosen_sa = st.multiselect(label="Sales Area", options=options_sa)
+
+
 curr_fy = chosen_fy
 
-filtered_omc_master = DataAnalysis.filter_data(df=st.session_state.omc_master, OMC=options_OMC)
+filtered_omc_master = DataAnalysis.filter_data(df=st.session_state.omc_master, OMC=options_OMC, SA=chosen_sa)
 curr_omc_sales = DataAnalysis.filter_data(df=st.session_state.omc_sales, Month=chosen_month, FiscalYear=curr_fy,
                                           PRODUCT=chosen_product)
 hist_fy = DataAnalysis.return_hist_fy(curr_fy)
@@ -37,12 +45,15 @@ mkt_sh = mkt_sh.reset_index()
 # st.table(mkt_sh)
 # st.bar_chart(data=mkt_sh, y='Mkt Sh Diff')
 
-fig = ff.create_table(mkt_sh)
-st.plotly_chart(fig, use_container_width=True)
+table = ff.create_table(mkt_sh)
+st.plotly_chart(table, use_container_width=True)
 
 fig = px.bar(data_frame=mkt_sh, x='OMC', y='Mkt Sh Diff', text_auto=True, color='OMC',
              color_discrete_map={
                  "HPC": "blue",
                  "BPC": "yellow",
                  "IOC": "red"})
-st.plotly_chart(fig, use_container_width=True)
+# st.plotly_chart(fig, use_container_width=True)
+
+selected_points = plotly_events(fig)
+st.write(selected_points[0]["x"])
